7b82a323de442fab8db85dbc3402d592ce213b0c
Nilstrieb <nils.heydecker@gmx.ch>
2020-12-04 23:37:46 +0100

    Added JavaDoc

--- src/main/java/Demo/Main.java
+++ src/main/java/Demo/Main.java
@@ -1,29 +1,28 @@
 package Demo;
 
+import Requests.Categories;
 import Requests.OpenTDB;
 
 import javax.security.auth.login.LoginException;
 
 public class Main {
 
-    public static void main(String[] Args) throws LoginException {
+    public static void main(String[] Args) {
         OpenTDB obj = new OpenTDB();
-        obj.setCategory(29);
+        obj.setCategory(Categories.ANY_CATEGORY);
         obj.setDifficulty("easy");
 
         obj.getTrivia();
 
-        System.out.println(obj.getQuestion());
-        System.out.println(obj.getCorrectAnswer());
+        System.out.println("q: " + obj.getQuestion());
+        System.out.println("c: " + obj.getCorrectAnswer());
 
-        String[] incorrectAnswers = obj.getIncorrectAnswers();
-        for (int i = 0; i < obj.incorrectAnswers.length ; i++) {
-            System.out.println(obj.incorrectAnswers[i]);
+        for (String s : obj.getIncorrectAnswers()) {
+            System.out.println("f:" + s);
         }
 
-        System.out.println(obj.getCategory());
-        System.out.println(obj.getDifficulty());
-        System.out.println(obj.getType());
-
+        System.out.println("cat: " + obj.getCategory());
+        System.out.println("dif: " + obj.getDifficulty());
+        System.out.println("typ: " + obj.getType());
     }
 }
