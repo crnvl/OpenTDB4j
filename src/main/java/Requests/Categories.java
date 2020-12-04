6db6d79918bac6154c1b59c8f571afeae629c41b
Nilstrieb <nils.heydecker@gmx.ch>
2020-12-04 23:38:12 +0100

    Added category constants

---
+++ src/main/java/Requests/Categories.java
@@ -0,0 +1,32 @@
+package Requests;
+
+/**
+ * The different categories and the corresponding int for the OpenTDB request
+ */
+public class Categories {
+    public static final int ANY_CATEGORY = 0;
+    public static final int GENERAL_KNOWLEDGE = 9;
+    public static final int ENTERTAINMENT_BOOKS = 10;
+    public static final int ENTERTAINMENT_FILM = 11;
+    public static final int ENTERTAINMENT_MUSIC = 12;
+    public static final int ENTERTAINMENT_MUSICALS_THEATRES = 13;
+    public static final int ENTERTAINMENT_TELEVISION = 14;
+    public static final int ENTERTAINMENT_VIDEO_GAMES = 15;
+    public static final int ENTERTAINMENT_BOARD_GAMES = 16;
+    public static final int SCIENCE_NATURE = 17;
+    public static final int SCIENCE_COMPUTERS = 18;
+    public static final int SCIENCE_MATHEMATICS = 19;
+    public static final int MYTHOLOGY = 20;
+    public static final int SPORTS = 21;
+    public static final int GEOGRAPHY = 22;
+    public static final int HISTORY = 23;
+    public static final int POLITICS = 24;
+    public static final int ART = 25;
+    public static final int CELEBRITIES = 26;
+    public static final int ANIMALS = 27;
+    public static final int VEHICLES = 28;
+    public static final int ENTERTAINMENT_COMICS = 29;
+    public static final int SCIENCE_GADGETS = 30;
+    public static final int ENTERTAINMENT_JAPANESE_ANIME_MANGA = 31;
+    public static final int ENTERTAINMENT_CARTOON_ANIMATIONS = 32;
+}
